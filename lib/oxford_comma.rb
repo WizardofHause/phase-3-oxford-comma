396033a914a73b6021fd60849d7b18126e70aba0
WizardofHause <jonrhause@gmail.com>
2022-11-08 22:01:17 -0700

    commit tree f44279cd47dbdd1d536de655d56f09b2d4804e19

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if (array.length <= 2)
+        array.join(" and ")
+    elsif (array.length >= 3) 
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["dave", "tom"])
+puts oxford_comma(["dave", "tom", "morris"])
+
